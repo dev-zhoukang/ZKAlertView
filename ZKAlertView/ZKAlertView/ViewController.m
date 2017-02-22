d9d7c77d3363c6a8a59f4ec0be2598ed693a43d8
ZK <dev.zhoukang@gmail.com>
2017-02-22 18:11:08 +0800

    init pro

---
+++ ZKAlertView/ZKAlertView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZKAlertView
+//
+//  Created by ZK on 17/2/22.
+//  Copyright © 2017年 ZK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ZKAlertView/ZKAlertView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZKAlertView
+//
+//  Created by ZK on 17/2/22.
+//  Copyright © 2017年 ZK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
